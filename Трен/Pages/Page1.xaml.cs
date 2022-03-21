31a4b7f76efd0ac3d5795f1f91a2cd0099e4ee21
Siropchik007 <stas.koval.89@mail.ru>
2022-03-21 09:32:35 +0300

    Добавьте файлы проекта.

---
+++ Трен/Pages/Page1.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace Трен.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+            
+        }
+
+
+
+        private void btnSqr_Click_1(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = Window.GetWindow(this) as MainWindow;
+            mainWindow.FrmMain.Navigate(new Pages.Page2());
+        }
+
+        private void btnLine_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = Window.GetWindow(this) as MainWindow;
+            mainWindow.FrmMain.Navigate(new Pages.Page3());
+        }
+    }
+}
